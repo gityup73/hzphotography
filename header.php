388ec15ba8f5cee200cbce0cdf3e4ccc7927c238
Jessica Swirat <soxfan1173@gmail.com>
2017-04-24 16:03:07 -0400

    more meta and css

--- header.php
+++ header.php
@@ -2,10 +2,11 @@
 <html>
 
 <head>
-	<title>Heather Zindars Photography</title>
+	<title>Heather Zindars Photography | Charlotte Photographer </title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="description" content="Heather Zindars is a Charlotte, NC photographer specializing in Family Photography, Newborn Photography, Child Photography, Sports Photography, Fashion Photography">
+    <meta name="robots" content="index,follow">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"> 
@@ -13,6 +14,7 @@
     <!-- End of Bootstrap css files -->
 
     <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="heather.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
